6c1f712bad9368bcfcbb19b76a96be3f591a9322
codermoksedul@gmail.com <dmoksedulislam@gmail.com>
2025-05-16 00:51:48 +0600

    add

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Derham Shop",
@@ -28,11 +17,7 @@ export default function RootLayout({
         {/* no index */}
         <meta name="robots" content="noindex" />
       </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={` antialiased`}>{children}</body>
     </html>
   );
 }
